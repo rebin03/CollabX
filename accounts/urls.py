bdcfa5c6399649cff2e0879627694a6b7d1c559e
Rebin <muhammedrebin1794@gmail.com>
2024-12-08 23:22:12 +0530

    ⬆️  Updatedurls

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('brand/signup/', views.BrandSignUpView.as_view(), name='brand-signup'),
+    path('brand/profile/add/', views.BrandProfileCreateView.as_view(), name='create-brand-profile'),
+    path('verify/email/', views.VerifyEmailView.as_view(), name='verify-email'),
+]
