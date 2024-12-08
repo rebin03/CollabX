e065c9d5ba8df09587e491ab82f78869caacf4c2
Rebin <muhammedrebin1794@gmail.com>
2024-12-08 15:34:27 +0530

    🎨 Designed data models for creator, brand and campaign

---
+++ accounts/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+
+    
+class User(AbstractUser):
+    otp = models.CharField(max_length=20, null=True, blank=True)
+    phone = models.CharField(max_length=10, null=True, unique=True)
+
+
+class Profile(BaseModel):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='media/profile_pics', null=True, blank=True)
+    contact_detail = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.full_name
+
+
+class Niche(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Platform(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    
+class CreatorProfile(BaseModel):
+    profile_object = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='creator_profile')
+    niche = models.ManyToManyField(Niche)
+    platform = models.ManyToManyField(Platform)
+    engagement_rate = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    past_collaboration = models.ManyToManyField('campaigns.Campaign')
+    
+
+class BrandType(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class BrandIndustry(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class BrandProfile(BaseModel):
+    profile_object = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='brand_profile')
+    brand_name = models.CharField(max_length=50, null=False, blank=False)
+    website = models.CharField(max_length=250, null=True, blank=True)
+    brand_type = models.ForeignKey(BrandType, on_delete=models.SET_NULL, null=True)
+    brand_industry = models.ForeignKey(BrandIndustry, on_delete=models.SET_NULL, null=True)
+    campaign_posted = models.ManyToManyField('campaigns.Campaign')
---
+++ campaigns/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_active=models.BooleanField(default=True)
+
+
+class ContentType(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Audience(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Campaign(BaseModel):
+    brand = models.ForeignKey('accounts.BrandProfile', on_delete=models.CASCADE, related_name='campaign')
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField()
+    picture = models.ImageField(upload_to='media/campaign_images', null=True, blank=True)
+    budget = models.FloatField()
+    content_types = models.ManyToManyField(ContentType)
+    audience_preferences = models.ManyToManyField(Audience)
+    requirements = models.TextField()
+    region = models.CharField(max_length=100)
+    
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+        ('close', 'Closed'),
+    )
+    
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Active')
