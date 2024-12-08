3961753f9712368f9c9b9166d99a5386d982ac83
Rebin <muhammedrebin1794@gmail.com>
2024-12-08 23:23:49 +0530

    📝 Registered model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from accounts.models import BrandIndustry, BrandType, Niche, Platform, Profile
 
 # Register your models here.
+
+admin.site.register(Niche)
+admin.site.register(Platform)
+admin.site.register(BrandType)
+admin.site.register(BrandIndustry)
+admin.site.register(Profile)
--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from campaigns.models import Audience, ContentType
 
 # Register your models here.
+
+admin.site.register(ContentType)
+admin.site.register(Audience)
